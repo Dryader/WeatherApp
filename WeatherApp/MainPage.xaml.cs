c82ad6fb4b2faffa2ed91be7a42e329bddf63a5f
Yuval <@>
2024-04-04 20:39:45 -0400

    sample weather data started, api calls working

--- WeatherApp/MainPage.xaml.cs
+++ WeatherApp/MainPage.xaml.cs
@@ -20,4 +20,15 @@ public partial class MainPage : ContentPage
 
         SemanticScreenReader.Announce(CounterBtn.Text);
     }
+    // https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=af814f7c81ec8ac0ad157b953140d72e
+
+    private async void OnWeatherClicked(object sender, EventArgs e)
+    {
+        var url =
+            "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=af814f7c81ec8ac0ad157b953140d72e";
+        var client = new HttpClient();
+        var response = await client.GetAsync(url);
+        var data = await response.Content.ReadAsStringAsync();
+        WeatherLabel.Text = data;
+    }
 }
