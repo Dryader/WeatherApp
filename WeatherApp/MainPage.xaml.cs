97dfdb9f8470153773604a3288bf5f0cce67c6d3
Yuval <@>
2024-04-11 19:50:16 -0400

    api calls fully working

--- WeatherApp/MainPage.xaml.cs
+++ WeatherApp/MainPage.xaml.cs
@@ -24,18 +24,19 @@ public partial class MainPage : ContentPage
     {
         var postalCode = cityEntry.Text;
         var url =
-            $"http://api.openweathermap.org/geo/1.0/direct?q={postalCode}&limit=5&appid=af814f7c81ec8ac0ad157b953140d72e";
+            $"http://api.openweathermap.org/geo/1.0/zip?zip={postalCode},CA&appid=af814f7c81ec8ac0ad157b953140d72e";
         var client = new HttpClient();
         var response = await client.GetAsync(url);
         var data = await response.Content.ReadAsStringAsync();
-        var locationData = JsonSerializer.Deserialize<List<LocationData>>(data);
-        cityEntry.Text = locationData[0].name;
+        var locationData = JsonSerializer.Deserialize<LocationData>(data);
+        GetWeatherData(locationData.lat, locationData.lon);
+        cityEntry.Text = locationData.name;
     }
 
     private class LocationData
     {
         public string zip { get; set; }
-        public string name { get; }
+        public string name { get; set; }
         public double lat { get; set; }
         public double lon { get; set; }
         public string country { get; set; }
