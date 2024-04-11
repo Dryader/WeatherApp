918bd4d255e44f6753ab0bcc4c082407afda67b3
Yuval <@>
2024-04-11 18:38:37 -0400

    setting up api calls

--- WeatherApp/MainPage.xaml.cs
+++ WeatherApp/MainPage.xaml.cs
@@ -1,34 +1,43 @@
-﻿namespace WeatherApp;
+﻿using System.Text.Json;
+
+namespace WeatherApp;
 
 public partial class MainPage : ContentPage
 {
-    private int count;
-
     public MainPage()
     {
         InitializeComponent();
+        GetLocationDataUsingPostalCode();
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
-    {
-        count++;
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
-    }
-    // https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=af814f7c81ec8ac0ad157b953140d72e
-
-    private async void OnWeatherClicked(object sender, EventArgs e)
+    private async void GetWeatherData(double latitude, double longitude)
     {
         var url =
-            "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=af814f7c81ec8ac0ad157b953140d72e";
+            $"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid=af814f7c81ec8ac0ad157b953140d72e";
         var client = new HttpClient();
         var response = await client.GetAsync(url);
         var data = await response.Content.ReadAsStringAsync();
-        WeatherLabel.Text = data;
+    }
+
+    private async void GetLocationDataUsingPostalCode()
+    {
+        var postalCode = cityEntry.Text;
+        var url =
+            $"http://api.openweathermap.org/geo/1.0/direct?q={postalCode}&limit=5&appid=af814f7c81ec8ac0ad157b953140d72e";
+        var client = new HttpClient();
+        var response = await client.GetAsync(url);
+        var data = await response.Content.ReadAsStringAsync();
+        var locationData = JsonSerializer.Deserialize<List<LocationData>>(data);
+        cityEntry.Text = locationData[0].name;
+    }
+
+    private class LocationData
+    {
+        public string zip { get; set; }
+        public string name { get; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public string country { get; set; }
     }
 }
