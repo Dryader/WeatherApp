463437c0df5f006270eb97ad3ee30d6fe9bc9ec8
Yuval <@>
2024-04-12 20:51:45 -0400

    .

--- WeatherApp/Pages/FiveDayForecast.xaml.cs
+++ WeatherApp/Pages/FiveDayForecast.xaml.cs
@@ -24,9 +24,6 @@ public partial class FiveDayForecast : ContentPage
         var forecastList = new List<fiveDayForecast.List>(ForecastData.list);
         var forecastTimes = new List<string>();
         var forecastTemps = new List<double>();
-        // var forecastList2 = new List<fiveDayForecast.List>(forecastTemps);
-        forecastListView.ItemsSource = forecastTemps.ToString();
-
 
         foreach (var forecast in forecastList)
             if (forecast.dt_txt.Contains("12:00:00"))
@@ -36,6 +33,8 @@ public partial class FiveDayForecast : ContentPage
                 forecastTimes.Add(day);
                 forecastTemps.Add(forecast.main.temp);
             }
+
+        forecastListView.ItemsSource = forecastTemps;
     }
 
     private async void GetLocationDataUsingPostalCode()
